05ae359996083e7bafe7f3ece4040c5735e03392
Praise Freedom Dike <dikepraise119@gmail.com>
2023-06-10 16:14:52 +0100

    create models

--- projectSpark/api/models.py
+++ projectSpark/api/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.username
+
+
+class Idea(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    idea = models.ForeignKey(Idea, on_delete=models.CASCADE)
+    commenter = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Comment on '{self.idea.title}' by {self.commenter.username}"
