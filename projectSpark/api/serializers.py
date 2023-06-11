726a57291f685350acca3e45072d83b655b1be48
Praise Freedom Dike <dikepraise119@gmail.com>
2023-06-11 17:35:58 +0100

    🚅 perfs: ignore pycache ✨feature: create serializers.py

---
+++ projectSpark/api/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import User, Idea, Comment
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'created_at', 'updated_at')
+
+class IdeaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Idea
+        fields = ('id', 'title', 'description', 'created_by', 'created_at', 'updated_at')
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ('id', 'idea', 'commenter', 'content', 'created_at', 'updated_at')
