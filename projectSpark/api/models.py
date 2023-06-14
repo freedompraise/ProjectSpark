5cbd0b901515de363b23ece88247fc0e936f0941
Praise Freedom Dike <dikepraise119@gmail.com>
2023-06-14 08:34:33 +0100

    Apply unique constraint in idea Title

--- projectSpark/api/models.py
+++ projectSpark/api/models.py
@@ -49,7 +49,7 @@ class User(AbstractBaseUser):
 
 
 class Idea(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, unique=True)
     description = models.TextField()
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
