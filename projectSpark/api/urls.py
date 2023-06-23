2d37945e12facc89640f68f1910757afc747d695
Praise Freedom Dike <dikepraise119@gmail.com>
2023-06-23 02:47:19 +0100

    create endpoint for IdeaRating

--- projectSpark/api/urls.py
+++ projectSpark/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import (
     UserRegistrationView, IdeaListAPIView, 
     IdeaDetailAPIView, CommentListCreateAPIView, CommentRetrieveUpdateDestroyAPIView, UserLoginView, 
-    TagListAPIView, IdeaListByTagAPIView
+    TagListAPIView, IdeaListByTagAPIView, IdeaRatingCreateAPIView, IdeaRatingListAPIView
 )
 
 urlpatterns = [
@@ -14,4 +14,5 @@ urlpatterns = [
     path('ideas/<int:idea_id>/comments/<int:pk>/', CommentRetrieveUpdateDestroyAPIView.as_view(), name='comment-detail'),
     path('tags/',TagListAPIView.as_view(), name = 'tag-list' ),
     path('ideas/<slug:tag_slug>/',IdeaListByTagAPIView.as_view(), name = 'idea-list-by-tag'),
+
 ]
