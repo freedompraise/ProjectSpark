ea77f6bb3d33e3b9a157f651909f74167198dcbe
Praise Freedom Dike <dikepraise119@gmail.com>
2023-08-16 03:45:00 +0100

    revert Title: Implement User Dashboard in Frontend #18

---
+++ projectSpark-frontend/app/page.js
@@ -0,0 +1,13 @@
+import style from "../styles/style.module.css";
+
+const Home = () => {
+  return (
+    <main className="w-full h-[100vh] flex justify-center items-center">
+      <h1 className="text-5xl text_gradient font-">
+      ProjectSpark || Ignite Your Ideas, Empower Your Projects 
+      </h1>
+    </main>
+  );
+};
+
+export default Home;
